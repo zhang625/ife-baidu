5ea43be71b9731736464141e6cd60ae061bf1ba7
zx625947@qq.com <zx625947@qq.com>
2016-10-11 09:46:03 +0800

    finish task30,and hava a function will do

--- task-30/task-30.js
+++ task-30/task-30.js
@@ -1,9 +1,11 @@
 // 一些变量
 var formId = document.getElementById('formTest');
 var verifyButton = document.getElementById('verify');
+var inputs = document.getElementsByTagName('input');
+var status = false;
 // 铁打的ie
 var EventUnit = {
-	addEventHandle : function (element,type,handle) {
+	 addEventHandle : function (element,type,handle) {
 		if (element.addEventListener) {
 			element.addEventListener(type,handle,false);
 		} else if(element.attachEvent){
@@ -11,21 +13,8 @@ var EventUnit = {
 		} else {
 			element['on'+type] = handle;
 		}
-	},
-	getEvent : function(event) {
-		return event?event : window.event;
-	},
-	getTarget : function(event) {
-		return event.target || event.srcElement;
-	},
-	preventDefault : function(event) {
-		if (event.preventDefault) {
-			event.preventDefault();
-		} else {
-			event.returnValue = false;
-		}
 	}
-}
+};
 
 // 将样式转换抽象为一个函数，这样能省一些代码
 function changeStyle (condition,element1,element2,element3) {
@@ -40,64 +29,66 @@ function changeStyle (condition,element1,element2,element3) {
 	}
 }
 
-// 创建一个构造函数，返回几个DOM节点
-
-// 验证函数
-// function verifyFn (event){
-	
-// }
-
-// 焦点聚集时候触发
-EventUnit.addEventHandle(formId,'focus',function(event){
-	let e = EventUnit.getEvent(event);
-	let target = EventUnit.getTarget(e);
-	let thisParent = target.parentNode;
-	let inputElement = thisParent.getElementsByTagName('input')[0];
-	let remindInputStyle = thisParent.getElementsByClassName('remind')[0];
-	// let remindErrorStyle = thisParent.getElementsByClassName('theError')[0];
-	// let remindSureStyle = thisParent.getElementsByClassName('theCorrect')[0];
-	if (target.tagName.toUpperCase() != 'INPUT') {
-		return;
-	};
-	remindInputStyle.style.display = 'block';
-})
-
-// 失去焦点时的
-EventUnit.addEventHandle(formId,'blur',function(event) {
-	let e = EventUnit.getEvent(event);
-	let target = EventUnit.getTarget(e);
-	let thisParent = target.parentNode;
-	let inputElement = thisParent.getElementsByTagName('input')[0];
+// 焦点聚集时的函数
+function blursFn (id){
+	let thisParent = document.getElementById(id)
 	let remindInputStyle = thisParent.getElementsByClassName('remind')[0];
 	let remindErrorStyle = thisParent.getElementsByClassName('theError')[0];
 	let remindSureStyle = thisParent.getElementsByClassName('theCorrect')[0];
-	let blurObj = new GetElement(that);
-	// 判断是不是点击的button，但是我觉得应该有更好的方法
-	if (target.tagName.toUpperCase() != 'INPUT') {
-		return;
-	};
+	remindInputStyle.style.display = 'block';
+	remindErrorStyle.style.display = 'none';
+	remindSureStyle.style.display = 'none';
+}
+// 失去焦点时触发的验证函数
+function verifyFn (pId,cId){
+	status = true;
+	let thisParent = document.getElementById(pId);
+	let inputElement = document.getElementById(cId);
+	let remindInputStyle = thisParent.getElementsByClassName('remind')[0];
+	let remindErrorStyle = thisParent.getElementsByClassName('theError')[0];
+	let remindSureStyle = thisParent.getElementsByClassName('theCorrect')[0];
 	remindInputStyle.style.display = 'none';
 	// 表单验证
-	switch(thisParent.id) {
+	switch(pId) {
 		case 'lengthtext' :
 			let lengthcond = Boolean(inputElement.value.trim().length > 4&&inputElement.value.trim().length<11);
+			status = lengthcond;
 			changeStyle(lengthcond,remindSureStyle,remindErrorStyle,inputElement);
 			break;
 		case 'nametest' :
 			let nametestcond = Boolean(inputElement.value.trim().length > 0);
+			status = nametestcond;
 			changeStyle(nametestcond,remindSureStyle,remindErrorStyle,inputElement);
 			break;
 		case 'formattest' :
 			let testStr = /[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/;
 			let formattestcond = testStr.test(inputElement.value.trim());
+			status = formattestcond;
 			changeStyle(formattestcond,remindSureStyle,remindErrorStyle,inputElement);
 			break;
 		default:
 			console.log('hava some error');
 			break;
 	}
-});
+}
+
+// 利用函数将每个的input元素来进行绑定事件
+[].forEach.call(inputs,function(item,index){
+	let thisId = item.getAttribute('id');
+	let parentId = thisId.slice(1);
+	let thisElement = document.getElementById(thisId);
+	EventUnit.addEventHandle(thisElement,'focus',function(){
+		blursFn(parentId);
+	});
+	EventUnit.addEventHandle(item,'blur',function() {
+		verifyFn(parentId,thisId);
+	});
+})
 
 EventUnit.addEventHandle(verifyButton,'click',function(event){
-
+	[].forEach.call(inputs,function(item,index){
+		let thisId = item.getAttribute('id');
+		let parentId = thisId.slice(1);
+		verifyFn(parentId,thisId);
+	})
 })
